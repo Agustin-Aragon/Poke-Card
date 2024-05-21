1f772cdea11abff82f8fb47584de8bb534da7305
Agustin Aragon <agusaragon35@gmail.com>
2024-05-21 19:58:29 -0300

    poke card

---
+++ script.js
@@ -0,0 +1,19 @@
+const myAtropos = Atropos({
+    el: '.my-atropos',
+    rotateXMax: 25,
+    rotateYMax: 25,
+    shadow: false,
+    highlight: false,
+  });
+
+
+  function completarDatos(id, foto, nombre, hp, atk, def, satk, sdef, vel) {
+    
+    
+
+
+
+
+
+
+  }
